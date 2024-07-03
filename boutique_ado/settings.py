d34d2855d73d82328e38c9e2277a127aecddeb21
Sean Littlewood <seanlitt80@gmail.com>
2024-07-03 12:31:14 +0000

    added shopping bag app urls and template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-seanl80-boutiqueadov2-hir1lhsmd2b.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-seanl80-boutiqueadov2-h0tevlv2sr3.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
